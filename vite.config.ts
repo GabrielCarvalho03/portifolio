fcdd7b12825b97f9e37973beaceb418d57888a5c
Gabriel <gabriel.carvalho@beelegal.com>
2024-09-02 15:45:14 -0300

    node resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 import path from "path";
 
 export default defineConfig({
@@ -21,5 +23,5 @@ export default defineConfig({
   define: {
     global: {},
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
