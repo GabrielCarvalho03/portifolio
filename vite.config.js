fd85198857c59503065cde66bc490b3eae05c681
Gabriel <gabriel.carvalho@beelegal.com>
2024-09-02 15:48:50 -0300

    node resolution

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@core": path.resolve(__dirname, "./src/core"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@domain": path.resolve(__dirname, "./src/domain"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@services": path.resolve(__dirname, "./src/services"),
+        },
+    },
+    server: {
+        port: 3000,
+    },
+    define: {
+        global: {},
+    },
+    plugins: [react(), tsconfigPaths()],
+});
